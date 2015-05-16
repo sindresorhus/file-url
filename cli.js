162bd76574cf34a9950852a920c0c1aa4fc84ff5
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-05-16 11:52:07 +0200

    Close #1 PR: Use `meow` in CLI.

--- cli.js
+++ cli.js
@@ -1,32 +1,17 @@
 #!/usr/bin/env node
 'use strict';
-var pkg = require('./package.json');
+var meow = require('meow');
 var fileUrl = require('./');
-var argv = process.argv.slice(2);
-var input = argv[0];
 
-function help() {
-	console.log([
+var cli = meow({
+	help: [
+		'Usage',
+		'  file-url [path]',
 		'',
-		'  ' + pkg.description,
-		'',
-		'  Usage',
-		'    file-url [path]',
-		'',
-		'  Example',
-		'    file-url',
-		'    file:///Users/sindresorhus/dev/file-url'
-	].join('\n'));
-}
+		'Example',
+		'  file-url',
+		'  file:///Users/sindresorhus/dev/file-url'
+	].join('\n')
+});
 
-if (argv.indexOf('--help') !== -1) {
-	help();
-	return;
-}
-
-if (argv.indexOf('--version') !== -1) {
-	console.log(pkg.version);
-	return;
-}
-
-console.log(fileUrl(input || process.cwd()));
+console.log(fileUrl(cli.input[0] || process.cwd()));
