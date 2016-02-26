6d3e526ab02a1a8f635bdb019a7589820015dc63
Felix Becker <felix.b@outlook.com>
2016-02-26 11:13:54 +0100

    Fix tests on Windows

--- test.js
+++ test.js
@@ -4,6 +4,11 @@ var fileUrl = require('./');
 
 it('should convert path to file url', function () {
 	assert(/file:\/\/\/.*\/test\.jpg/.test(fileUrl('test.jpg')));
+	if (process.platform === 'win32') {
+		assert.strictEqual(fileUrl('C:\\Users\\sindresorhus\\dev\\te^st.jpg'), 'file:///C:/Users/sindresorhus/dev/te%5Est.jpg');
+	} else {
+		assert.strictEqual(fileUrl('/Users/sindresorhus/dev/te^st.jpg'), 'file:///Users/sindresorhus/dev/te%5Est.jpg');
+	}
 });
 
 it('should accept resolve parameter', function () {
