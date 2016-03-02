c77fefb96adee8d5a7e1a42cb2c257dafc10e9e2
Felix Becker <felix.b@outlook.com>
2016-03-02 16:04:35 +0700

    Close #3 PR: Add `resolve` option. Fixes #2

--- test.js
+++ test.js
@@ -4,5 +4,13 @@ var fileUrl = require('./');
 
 it('should convert path to file url', function () {
 	assert(/file:\/\/\/.*\/test\.jpg/.test(fileUrl('test.jpg')));
-	assert.strictEqual(fileUrl('/Users/sindresorhus/dev/te^st.jpg'), 'file:///Users/sindresorhus/dev/te%5Est.jpg');
+	if (process.platform === 'win32') {
+		assert.strictEqual(fileUrl('C:\\Users\\sindresorhus\\dev\\te^st.jpg'), 'file:///C:/Users/sindresorhus/dev/te%5Est.jpg');
+	} else {
+		assert.strictEqual(fileUrl('/Users/sindresorhus/dev/te^st.jpg'), 'file:///Users/sindresorhus/dev/te%5Est.jpg');
+	}
+});
+
+it('should accept resolve parameter', function () {
+	assert.strictEqual(fileUrl('test.jpg', {resolve: false}), 'file:///test.jpg');
 });
